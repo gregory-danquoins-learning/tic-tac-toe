6fa2e7f9d9bff2177203dfa04672cc17a0d7bd51
Grégory Danquoins <152988641+Gregory-Danquoins@users.noreply.github.com>
2025-09-08 09:32:50 +0200

    modification commentaire

--- public/game.js
+++ public/game.js
@@ -1,4 +1,4 @@
-// Récupération du nom du joueur depuis le localStorage.
+// Récupération du nom du joueur dans localStorage.
 const playerName = localStorage.getItem("username");
 if (!playerName) window.location.href = "/login.html";
 
@@ -6,30 +6,29 @@ if (!playerName) window.location.href = "/login.html";
 const gameId = localStorage.getItem("gameId");
 if (!gameId) window.location.href = "/lobby.html";
 
-// Variables globales pour la partie
+// Variables globales de la partie
 let socket;      // La connexion WebSocket avec le serveur
-let symbol;      // Le symbole du joueur ("X" ou "O")
+let symbol;      // Symbole du joueur ("X" ou "O")
 let yourTurn = false; // Indique si c’est au joueur actuel de jouer
 
 // Affichage du nom du joueur dans la page
 document.getElementById("playerName").textContent = playerName;
 
-// Fonction qui crée le plateau de jeu (3x3)
-// Elle reçoit l’état actuel du board (tableau 2D).
+// Fonction qui crée le plateau de jeu , elle reçoit l’état actuel du board 
 function createBoard(board) {
   const container = document.getElementById("board");
-  container.innerHTML = ""; // On vide l’ancien plateau
+  container.innerHTML = ""; // On vide l’ancien board
 
-  // Double boucle pour parcourir les lignes et colonnes
+  // boucle pour parcourir les lignes et colonnes
   for (let row = 0; row < 3; row++) {
     for (let col = 0; col < 3; col++) {
       const cell = document.createElement("div"); // Création d’une case
-      cell.className = "cell"; // Style CSS appliqué
+      cell.className = "cell"; // Style CSS
       cell.dataset.row = row;  // Sauvegarde la position (ligne)
       cell.dataset.col = col;  // Sauvegarde la position (colonne)
       cell.textContent = board[row][col]; // Affiche "X", "O" ou vide
 
-      // Si la case est vide, on autorise le clic pour jouer
+      // Si la case est vide, on autorise le clic 
       if (board[row][col] === "") {
         cell.addEventListener("click", () => play(row, col));
       }
@@ -39,14 +38,14 @@ function createBoard(board) {
   }
 }
 
-// Met à jour le texte de statut 
+// Met à jour le  statut 
 function updateStatus(text) {
   document.getElementById("status").textContent = text;
 }
 
 // Fonction déclenchée quand un joueur clique sur une case
 function play(row, col) {
-  // On bloque si ce n’est pas notre tour
+  // On bloque si ce n’est pas son tour
   if (!yourTurn) return;
 
   // On envoie la position jouée au server 
